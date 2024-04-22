d929ae5f36d88fe916367a74cb07c640ba363039
anshupandey <anshupandey>
2024-04-22 05:50:11 +0000

    DB notebook for data factory

---
+++ Code10_Databricks_notebook_for_Data_Factory.py
@@ -0,0 +1,21 @@
+# Databricks notebook source
+# create folder
+dbutils.widgets.text("folder", "mydata")
+
+#get the foldername
+folder = dbutils.widgets.get("folder")
+
+# COMMAND ----------
+
+import urllib3
+
+# download some data and save that data into the specified folder
+
+
+url = "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv"
+response = urllib3.PoolManager().request('GET',url)
+data = response.data.decode("utf-8")
+
+#save data
+path = "dbfs:/{0}/products.csv".format(folder)
+dbutils.fs.put(path,data,True)
